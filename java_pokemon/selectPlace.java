c6aeebea8f8dd09650365c463ffa4fb3afc0902a
Jon Landa <jonlanda06@gmail.com>
2022-04-22 12:51:18 +0200

    started menu

---
+++ java_pokemon/selectPlace.java
@@ -0,0 +1,16 @@
+package java_pokemon;
+import java.util.Scanner;
+
+public class selectPlace {
+    public static void selectPlace() {
+        System.out.println("Welcome to Pokemon-Java");
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Where do you want to go?");
+        System.out.println("(1) Lake");
+        System.out.println("(2) Forest");
+        System.out.println("(3) Field");
+        System.out.println("(4) Mountain");
+        System.out.println("(5) Cave");
+        int ortw = myObj.nextInt();
+    }
+}
