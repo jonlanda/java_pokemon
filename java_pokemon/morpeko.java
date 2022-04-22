720a4c48070887472c4a7b13f95aa24b7ca03b60
Jon Landa <jonlanda06@gmail.com>
2022-04-22 11:29:32 +0200

    Made attacks for Morpeko

--- java_pokemon/morpeko.java
+++ java_pokemon/morpeko.java
@@ -2,4 +2,7 @@ package java_pokemon;
 
 public class morpeko {
     pokemon morpeko = new pokemon("Morpeko", 35, 1, "Electric");
+    attack attack1 = new attack("Famished", 0, -1, "Water");
+    attack attack2 = new attack("Spark", 10, 2, "Water");
+    attack attack3 = new attack("Aura Wheel", 17, 4, "Water");
 }
