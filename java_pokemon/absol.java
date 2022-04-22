698f27d380e2f42fe6340db94ede6a4c54b2fd58
Denis <Denis.Meyer@gmx.ch>
2022-04-22 11:29:31 +0200

    absol attacks

--- java_pokemon/absol.java
+++ java_pokemon/absol.java
@@ -2,4 +2,8 @@ package java_pokemon;
 
 public class absol {
     pokemon absol = new pokemon("Absol", 45, 1, "Dark");
+    attack attack1 = new attack("Quick Attack", 5, 0, "Ghost");
+    attack attack2 = new attack("Night Slash", 10, 1, "Ghost");
+    attack attack3 = new attack("Dark Pulse", 15, 2, "Ghost");
+
 }
