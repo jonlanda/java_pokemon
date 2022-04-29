c6d999d729062bb3dd29e5089f5bccbc25f63507
Jon Landa <jonlanda06@gmail.com>
2022-04-29 09:16:59 +0200

    Catch Pokemon not working

--- java_pokemon/Morpeko.java
+++ java_pokemon/Morpeko.java
@@ -5,7 +5,7 @@ public class Morpeko extends Pokemon {
     public Morpeko(String name, int health, int energy, int level) {
         super(name, health, energy, level);
 
-        this.attacks.add(new Attack("Famished", 0, 0, "Water"));
+        this.attacks.add(new Attack("Famished", 6, 0, "Water"));
         this.attacks.add(new Attack("Spark", 9, 2, "Water"));
         this.attacks.add(new Attack("Aura Wheel", 14, 4, "Water"));
     }
