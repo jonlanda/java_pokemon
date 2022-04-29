a3f630d12b5b9ef14497da3ffe5cfca4aaf4607f
Denis <Denis.Meyer@gmx.ch>
2022-04-29 08:21:08 +0200

    whatever

--- java_pokemon/fight.java
+++ java_pokemon/fight.java
@@ -10,18 +10,21 @@ public class fight extends Trainer {
         System.out.println("Your Attacks: ");
         team.get(PkmW).showAttacks();
         int AtkW = myAtk.nextInt();
-        switch(AtkW) {
+        switch (AtkW) {
             case 1:
                 team.get(PkmW).attack(0, spawnedPokemon);
-                System.out.println("The wild " + spawnedPokemon.getName() + " has " + spawnedPokemon.getHealth() + " Health left!");
+                System.out.println("The wild " + spawnedPokemon.getName() + " has " + spawnedPokemon.getHealth()
+                        + " Health left!\n");
                 attackEnemy(spawnedPokemon, PkmW);
             case 2:
                 team.get(PkmW).attack(1, spawnedPokemon);
-                System.out.println("The wild " + spawnedPokemon.getName() + " has " + spawnedPokemon.getHealth() + " Health left!");
+                System.out.println("The wild " + spawnedPokemon.getName() + " has " + spawnedPokemon.getHealth()
+                        + " Health left!\n");
                 attackEnemy(spawnedPokemon, PkmW);
             case 3:
                 team.get(PkmW).attack(2, spawnedPokemon);
-                System.out.println("The wild " + spawnedPokemon.getName() + " has " + spawnedPokemon.getHealth() + " Health left!");
+                System.out.println("The wild " + spawnedPokemon.getName() + " has " + spawnedPokemon.getHealth()
+                        + " Health left!\n");
                 attackEnemy(spawnedPokemon, PkmW);
         }
     }
@@ -31,32 +34,33 @@ public class fight extends Trainer {
         System.out.println(" It has " + spawnedPokemon.getHealth() + " Health\n\n");
     }
 
-    public static void attackEnemy(Pokemon spawnedPokemon, int PkmW){
+    public static void attackEnemy(Pokemon spawnedPokemon, int PkmW) {
         int max = 15;
         int min = 1;
         int enemyAttack = (int) Math.floor(Math.random() * (max - min + 1) + min);
-        if(enemyAttack <= 8){
+        if (enemyAttack <= 8) {
             spawnedPokemon.atkEne(0, team.get(PkmW));
-            System.out.println(spawnedPokemon + " uses ");
+            System.out.println("The wild " + spawnedPokemon.getName() + " uses \n");
             System.out.println("Your " + team.get(PkmW).getName() + " has " + team.get(PkmW).getHealth() + " Health");
             attackYou(spawnedPokemon, PkmW);
-        } else if(enemyAttack > 8 && enemyAttack <= 12){
+        } else if (enemyAttack > 8 && enemyAttack <= 12) {
             spawnedPokemon.atkEne(1, team.get(PkmW));
 
             System.out.println("Your " + team.get(PkmW).getName() + " has " + team.get(PkmW).getHealth() + " Health");
             attackYou(spawnedPokemon, PkmW);
-        }else{
+        } else {
             spawnedPokemon.atkEne(2, team.get(PkmW));
             System.out.println("Your " + team.get(PkmW).getName() + " has " + team.get(PkmW).getHealth() + " Health");
             attackYou(spawnedPokemon, PkmW);
         }
 
     }
+
     public static void choosePokemon(Pokemon spawnedPokemon) {
         Scanner myPkm = new Scanner(System.in);
         System.out.println("What Pokemon do you want to use?");
         int b = 1;
-        for(int i = 0; i < team.size(); i++) {
+        for (int i = 0; i < team.size(); i++) {
             System.out.println(b + ") " + team.get(i).getName());
             b++;
         }
@@ -65,6 +69,6 @@ public class fight extends Trainer {
     }
 }
 
-//System.out.println(spawnedPokemon.getHealth());
-//team.get(0).attack(0, spawnedPokemon);
-//System.out.println(spawnedPokemon.getHealth());
+// System.out.println(spawnedPokemon.getHealth());
+// team.get(0).attack(0, spawnedPokemon);
+// System.out.println(spawnedPokemon.getHealth());
