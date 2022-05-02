6b636e4da152b835990754e5e47a0db0402950a1
Jon Landa <jonlanda06@gmail.com>
2022-05-02 13:54:56 +0200

    Fixed fight with champ

--- java_pokemon/Champ.java
+++ java_pokemon/Champ.java
@@ -7,6 +7,7 @@ import javax.lang.model.util.ElementScanner6;
 
 public class Champ {
     public static int count = 0;
+    public static int count1 = 0;
     public ArrayList<Pokemon> Champteam;
 
     public static void readyForChamp() {
@@ -21,14 +22,14 @@ public class Champ {
     }
 
     public static void fightChamp() {
-        System.out.println("Hello Trainer! Im Daddy Shake and im the Champ of Pokemon-Java! " +
-                "So you think you are strong enough to fight me?! Well let's see hehehe\n");
+        System.out.println(Colors.RED + "Hello Trainer! Im Daddy Shake and im the Champ of Pokemon-Java! " +
+                "So you think you are strong enough to fight me?! Well let's see hehehe\n" + Colors.RESET);
         System.out.println("Daddy Shake ");
-        choosePokemon();
+        champPokemon1();
 
     }
 
-    public static void choosePokemon() {
+    public static void choosePokemon(Pokemon spawnedPokemon) {
         Scanner myPkm = new Scanner(System.in);
         System.out.println("What Pokemon do you want to use?");
         int b = 1;
@@ -37,80 +38,49 @@ public class Champ {
             b++;
         }
         int PkmW = myPkm.nextInt() - 1;
-        champPokemon1(PkmW);
+        attackYouChamp(spawnedPokemon, PkmW);
     }
 
-    public static void champPokemon1(int PkmW) {
+    public static void champPokemon1() {
         Pokemon champPokemon1 = new Charizard("Charizard", 100, 19000, 25);
         System.out.println(
                 "Daddy Shake sends out " + Colors.RED + champPokemon1.getName() + Colors.RESET + " to battle.");
-        attackYouChamp(champPokemon1, PkmW);
+        choosePokemon(champPokemon1);
     }
 
-    public static void champPokemon2(int PkmW) {
+    public static void champPokemon2() {
         Pokemon champPokemon2 = new Alakazam("Alakazam", 85, 19000, 25);
         System.out.println(
                 "Daddy Shake sends out " + Colors.RED + champPokemon2.getName() + Colors.RESET + " to battle.");
-        attackYouChamp(champPokemon2, PkmW);
+        choosePokemon(champPokemon2);
     }
 
-    public static void champPokemon3(int PkmW) {
+    public static void champPokemon3() {
         Pokemon champPokemon3 = new Garchomp("Garchomp", 125, 19000, 25);
         System.out.println(
                 "Daddy Shake sends out " + Colors.RED + champPokemon3.getName() + Colors.RESET + " to battle.");
-        attackYouChamp(champPokemon3, PkmW);
+        choosePokemon(champPokemon3);
     }
 
-    public static void champPokemon4(int PkmW) {
+    public static void champPokemon4() {
         Pokemon champPokemon4 = new Aegislash("Aegislash", 120, 19000, 25);
         System.out.println(
                 "Daddy Shake sends out " + Colors.RED + champPokemon4.getName() + Colors.RESET + " to battle.");
-        attackYouChamp(champPokemon4, PkmW);
+        choosePokemon(champPokemon4);
     }
 
-    public static void attackEnemyChamp(Pokemon spawnedPokemon, int PkmW) {
-        int max = 15;
-        int min = 1;
-        int enemyAttack = (int) Math.floor(Math.random() * (max - min + 1) + min);
-        if (enemyAttack <= 8) {
-            spawnedPokemon.atkEne(0, fight.team.get(PkmW));
-            System.out.println("Daddy Shake's " + Colors.RED + spawnedPokemon.getName() + Colors.RESET + " uses "
-                    + spawnedPokemon.attacks.get(0).getAttackName() + " and deals "
-                    + spawnedPokemon.attacks.get(0).getDmg() + " damage! \n");
-            System.out.println("Your " + fight.team.get(PkmW).getName() + " has " + Colors.GREEN
-                    + fight.team.get(PkmW).getHealth() + Colors.RESET + " Health");
-            fight.diePokemon(PkmW, spawnedPokemon);
-            attackYouChamp(spawnedPokemon, PkmW);
-        } else if (enemyAttack > 8 && enemyAttack <= 12) {
-            spawnedPokemon.atkEne(1, fight.team.get(PkmW));
-            System.out.println("Daddy Shake's " + Colors.RED + spawnedPokemon.getName() + Colors.RESET + " uses "
-                    + spawnedPokemon.attacks.get(1).getAttackName() + " and deals "
-                    + spawnedPokemon.attacks.get(1).getDmg() + " damage! \n");
-            System.out.println("Your " + fight.team.get(PkmW).getName() + " has " + Colors.GREEN
-                    + fight.team.get(PkmW).getHealth() + Colors.RESET + " Health");
-            fight.diePokemon(PkmW, spawnedPokemon);
-            attackYouChamp(spawnedPokemon, PkmW);
-        } else {
-            spawnedPokemon.atkEne(2, fight.team.get(PkmW));
-            System.out.println("Daddy Shake's " + Colors.RED + spawnedPokemon.getName() + Colors.RESET + " uses "
-                    + spawnedPokemon.attacks.get(2).getAttackName() + " and deals "
-                    + spawnedPokemon.attacks.get(2).getDmg() + " damage! \n");
-            System.out.println("Your " + fight.team.get(PkmW).getName() + " has " + Colors.GREEN
-                    + fight.team.get(PkmW).getHealth() + Colors.RESET + " Health");
-            fight.diePokemon(PkmW, spawnedPokemon);
-
-            attackYouChamp(spawnedPokemon, PkmW);
+    public static void diePokemon(int PkmW, Pokemon spawnedPokemon) {
+        if (fight.team.get(PkmW).getHealth() <= 0) {
+            System.out.println("Oh no your " + fight.team.get(PkmW).getName() + " fainted!");
+            fight.team.remove(PkmW);
+            fight.dieTeam();
+            choosePokemon(spawnedPokemon);
         }
-
-    }
+        }
 
     public static void attackYouChamp(Pokemon spawnedPokemon, int PkmW) {
         fightIntroChamp(spawnedPokemon, PkmW);
         Scanner myAtk = new Scanner(System.in);
-
-        if (spawnedPokemon.getHealth() <= 0) {
-            champPokemon2(PkmW);
-        }
 
         System.out.println("Your Attacks: ");
         fight.team.get(PkmW).showAttacks();
@@ -125,11 +95,11 @@ public class Champ {
                 if (spawnedPokemon.getHealth() <= 0) {
                     count++;
                     if (count == 1) {
-                        champPokemon2(PkmW);
+                        champPokemon2();
                     } else if (count == 2) {
-                        champPokemon3(PkmW);
+                        champPokemon3();
                     } else if (count == 3) {
-                        champPokemon4(PkmW);
+                        champPokemon4();
                     } else {
                         System.out.println(
                                 "Congratulations! You are the new Pokemon-Java Champion! Jon and Denis are very proud of you! Thanks for playing. :)");
@@ -145,11 +115,11 @@ public class Champ {
                 if (spawnedPokemon.getHealth() <= 0) {
                     count++;
                     if (count == 1) {
-                        champPokemon2(PkmW);
+                        champPokemon2();
                     } else if (count == 2) {
-                        champPokemon3(PkmW);
+                        champPokemon3();
                     } else if (count == 3) {
-                        champPokemon4(PkmW);
+                        champPokemon4();
                     } else {
                         System.out.println(
                                 "Congratulations! You are the new Pokemon-Java Champion! Jon and Denis are very proud of you! Thanks for playing. :)");
@@ -166,11 +136,11 @@ public class Champ {
                 if (spawnedPokemon.getHealth() <= 0) {
                     count++;
                     if (count == 1) {
-                        champPokemon2(PkmW);
+                        champPokemon2();
                     } else if (count == 2) {
-                        champPokemon3(PkmW);
+                        champPokemon3();
                     } else if (count == 3) {
-                        champPokemon4(PkmW);
+                        champPokemon4();
                     } else {
                         System.out.println(
                                 "Congratulations! You are the new Pokemon-Java Champion! Jon and Denis are very proud of you! Thanks for playing. :)");
@@ -182,17 +152,53 @@ public class Champ {
         if (spawnedPokemon.getHealth() <= 0) {
             count++;
             if (count == 1) {
-                champPokemon2(PkmW);
+                champPokemon2();
             } else if (count == 2) {
-                champPokemon3(PkmW);
+                champPokemon3();
             } else if (count == 3) {
-                champPokemon4(PkmW);
+                champPokemon4();
             } else {
                 System.out.println(
                         "Congratulations! You are the new Pokemon-Java Champion! Jon and Denis are very proud of you! Thanks for playing. :)");
                 System.exit(0);
             }
         }
+    }
+
+    public static void attackEnemyChamp(Pokemon spawnedPokemon, int PkmW) {
+        int max = 15;
+        int min = 1;
+        int enemyAttack = (int) Math.floor(Math.random() * (max - min + 1) + min);
+        if (enemyAttack <= 8) {
+            spawnedPokemon.atkEne(0, fight.team.get(PkmW));
+            System.out.println("Daddy Shake's " + Colors.RED + spawnedPokemon.getName() + Colors.RESET + " uses "
+                    + spawnedPokemon.attacks.get(0).getAttackName() + " and deals "
+                    + spawnedPokemon.attacks.get(0).getDmg() + " damage! \n");
+            System.out.println("Your " + fight.team.get(PkmW).getName() + " has " + Colors.GREEN
+                    + fight.team.get(PkmW).getHealth() + Colors.RESET + " Health");
+            diePokemon(PkmW, spawnedPokemon);
+            attackYouChamp(spawnedPokemon, PkmW);
+        } else if (enemyAttack > 8 && enemyAttack <= 12) {
+            spawnedPokemon.atkEne(1, fight.team.get(PkmW));
+            System.out.println("Daddy Shake's " + Colors.RED + spawnedPokemon.getName() + Colors.RESET + " uses "
+                    + spawnedPokemon.attacks.get(1).getAttackName() + " and deals "
+                    + spawnedPokemon.attacks.get(1).getDmg() + " damage! \n");
+            System.out.println("Your " + fight.team.get(PkmW).getName() + " has " + Colors.GREEN
+                    + fight.team.get(PkmW).getHealth() + Colors.RESET + " Health");
+            diePokemon(PkmW, spawnedPokemon);
+            attackYouChamp(spawnedPokemon, PkmW);
+        } else {
+            spawnedPokemon.atkEne(2, fight.team.get(PkmW));
+            System.out.println("Daddy Shake's " + Colors.RED + spawnedPokemon.getName() + Colors.RESET + " uses "
+                    + spawnedPokemon.attacks.get(2).getAttackName() + " and deals "
+                    + spawnedPokemon.attacks.get(2).getDmg() + " damage! \n");
+            System.out.println("Your " + fight.team.get(PkmW).getName() + " has " + Colors.GREEN
+                    + fight.team.get(PkmW).getHealth() + Colors.RESET + " Health");
+            diePokemon(PkmW, spawnedPokemon);
+
+            attackYouChamp(spawnedPokemon, PkmW);
+        }
+
     }
 
     public static void fightIntroChamp(Pokemon spawnedPokemon, int PkmW) {
