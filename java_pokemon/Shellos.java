dc7a9bd3ee9358f73915ea88f0af4b2c0ca9fdb4
Jon Landa <jonlanda06@gmail.com>
2022-05-02 13:59:18 +0200

    ent-opd shellos

--- java_pokemon/Shellos.java
+++ java_pokemon/Shellos.java
@@ -5,7 +5,7 @@ public class Shellos extends Pokemon {
     public Shellos(String name, int health, int energy, int level) {
         super(name, health, energy, level);
 
-        this.attacks.add(new Attack("Tackle", 5000, 0, "Fire"));
+        this.attacks.add(new Attack("Tackle", 5, 0, "Fire"));
         this.attacks.add(new Attack("Water Pulse", 13, 2, "Ground"));
         this.attacks.add(new Attack("Hydro Pump", 20, 4, "Fire"));
     }
